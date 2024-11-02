ce8fd025053372ebc5dbef39aed307ec4a6b7339
vivianli1337 <vivianli1337@gmail.com>
2024-11-02 16:46:02 -0400

    initial folders

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { HashRouter, Route, Routes, Navigate } from "react-router-dom"; 
-// import logo from './logo.svg';
-// import './App.css';
+//import logo from './logo.svg';
+//import './App.css';
 
 function App() {
   return (
